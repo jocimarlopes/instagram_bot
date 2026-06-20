8a613a0ce9a4c322dba46834757e04ae6bef87b6
Jocimar Lopes <lopes.jocimar@gmail.com>
2026-06-19 23:58:22 -0300

    feat: changed to random interval seconds

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from auth import setup_instagram_client
 from monitor import monitor_latest_comments
 from database import init_db
+from random import randint
+
+MIN_CHECK_INTERVAL_SECONDS = 600  # 10 minutos
+MAX_CHECK_INTERVAL_SECONDS = 1800  # 30 minutos
 
 def main():
     print("🚀 Iniciando o Bot do Instagram com Integração de IA...")
@@ -15,7 +19,8 @@ def main():
 
     # 3. Inicia o loop infinito de monitoramento (monitor.py)
     # Definido para rodar a cada 300 segundos (5 minutos) para evitar bloqueios
-    monitor_latest_comments(cliente_insta, check_interval_seconds=300)
+    INTERVAL_SECONDS = randint(MIN_CHECK_INTERVAL_SECONDS, MAX_CHECK_INTERVAL_SECONDS)
+    monitor_latest_comments(cliente_insta, check_interval_seconds=INTERVAL_SECONDS)
 
 if __name__ == "__main__":
     try:
